35d4ac2c607210594cef0af4473626c03a26cca0
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-25 01:42:57 +0530

    feat : "add theme mode switch"

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,13 +1,28 @@
 import { useTheme } from "@emotion/react";
-import { Box, Divider, IconButton, Stack } from "@mui/material";
+import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material";
 import { Gear } from "phosphor-react";
 import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings" 
 
+// icons
 import Logo from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data";
+
+//MaterialUISwitch
+import MaterialUISwitch from "../../components/MaterialUISwitch";
+
+
 const DashboardLayout = () => {
-  const theme = useTheme();
+  const theme = useTheme(); 
+
+  // state for default selected button in sidebar
+  const [ selected, setSelected ] = useState(0);
+
+  // Themetoggle method from useSettings hook
+  const { onToggleMode } = useSettings();
 
   return (
     <>
@@ -22,29 +37,86 @@ const DashboardLayout = () => {
       >
         <Stack
           direction="column"
+          justifyContent={"space-between"}
           alignItems={"center"}
-          sx={{ width: "100%" }}
+          sx={{ height: "100%" }}
           spacing={3}
         >
-          <Box
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: 1.5,
-            }}
-          >
-            <img src={Logo} alt={"Chat App Logo"} />
-          </Box>
-          <Stack sx={{}} spacing={3}>
-            {Nav_Buttons.map((el) => (
-              <IconButton key={el.index}>{el.icon}</IconButton>
-            ))}
-            <Divider />
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+              }}
+            >
+              {/* App logo */}
+              <img src={Logo} alt={"Chat App Logo"} />
+            </Box>
+            <Stack sx={{}} spacing={3}>
+              {/* Nav_Buttons are imported from data folder (index.js) */}
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{ width: "max-content", color: theme.palette.mode === "light" ? "#000" : theme.palette.text.primary }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
 
-            <IconButton>
-              <Gear />
-            </IconButton>
+              {/* divider works like hr tag */}
+              <Divider sx={{ width: "44px" }} />
+              {selected === 3 ? (
+                <Box
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => setSelected(3)}
+                  sx={{ width: "max-content", color: theme.palette.mode === "light" ? "#000" : theme.palette.text.primary }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+
+          {/* Avatar & switch */}
+          <Stack spacing={4}>
+            <MaterialUISwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+            <Avatar src={faker.image.avatar()} />
           </Stack>
         </Stack>
       </Box>
