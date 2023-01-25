49759301057da2ee72a8f59024d47d9292d0b32f
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-26 01:31:36 +0530

    feat : "Conversation Box"

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,31 @@
+import {Stack, Box, useTheme } from "@mui/material";
 import React from "react";
+import Conversation from "../../components/Conversation";
 import Chats from "./Chats";
 
+// generalApp renders Chats and Conversation box
 const GeneralApp = () => {
+  const theme = useTheme();
 
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
+      {/* Chats */}
       <Chats />
-    </>
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "Calc(100vw - 420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        {/* Conversation Box */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
